9a5228af8bcd8be5517a563c4ea853bfa53930b5
Darshan Chobarkar <dchobarkar@gmail.com>
2025-04-14 13:14:44 +0530

    fix: allow Render to import src modules by updating PYTHONPATH

--- web/app.py
+++ web/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from src.data_loader import load_movielens_data
